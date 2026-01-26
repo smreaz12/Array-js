a59315b2511e67924c50424e33df24f95de023bd
smreaz12 <smreaz555@gmail.com>
2026-01-26 18:20:09 +0600

    check array or not array

---
+++ isarray.js
@@ -0,0 +1,9 @@
+let fruits = ["Apple", "orange", "Khormuz", "pepe"]
+
+let number = []
+
+let CGPA = 3.90;
+
+console.log(Array.isArray(fruits))
+console.log(Array.isArray(number))
+console.log(Array.isArray(CGPA))
