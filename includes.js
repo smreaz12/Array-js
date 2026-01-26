e641cb0c2f9d48a02b768960838d752a63f67b18
smreaz12 <smreaz555@gmail.com>
2026-01-26 18:08:53 +0600

    includes in array

---
+++ includes.js
@@ -0,0 +1,13 @@
+let fruits = ["Apple", "orange", "Khormuz", "pepe"]
+
+console.log(fruits.includes("Apple"));
+
+//
+
+if(fruits.includes("Apple")){
+    console.log("I want to eat Apple")
+}
+
+else{
+    console.log("Kicui khabo na")
+}
