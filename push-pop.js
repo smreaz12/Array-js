958c846b9d0799a721762c1ad8517d0681665f2f
smreaz12 <smreaz555@gmail.com>
2026-01-26 17:42:08 +0600

    push pop in array

---
+++ push-pop.js
@@ -0,0 +1,20 @@
+let num = [22,88,90,12,7,15]
+let fruits = ["Apple", "orange", "Khormuz", "pepe"]
+
+console.log(num,fruits);
+
+//push//
+
+num.push(555);
+console.log(num);
+
+fruits.push("kola", "angur");
+console.log(fruits);
+
+//pop//
+
+num.pop();
+console.log(num);
+
+fruits.pop()
+console.log(fruits);
