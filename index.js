3c039908a411d96a3b312c69a0ad2972b3724bb7
smreaz12 <smreaz555@gmail.com>
2026-01-26 17:25:43 +0600

    array index define

---
+++ index.js
@@ -0,0 +1,15 @@
+let num = [22,24,87,9,83,66,98,29,51,84,89,80,227,78];
+
+console.log(num[2]);
+
+console.log(num[7]);
+
+const fourth = num[10];
+console.log(fourth);
+
+
+//
+
+num[1] = 55;
+console.log(num);
+
