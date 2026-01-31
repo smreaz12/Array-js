e497d6f24e8ea483b9a6086c2430fed9dea2ac76
smreaz12 <smreaz555@gmail.com>
2026-01-31 15:40:07 +0600

    spread operator in js

---
+++ spread.js
@@ -0,0 +1,8 @@
+//---spread operator---//
+//best way to add multiple array in one array//
+let arr1 = [22,66,88,66];
+let arr2 = [54,89,90,12];
+let arr3 = ["reaz","rahin",true];
+
+let arr = [...arr1,...arr2,...arr3];
+console.log(arr);
--- test.js
+++ test.js
@@ -12,12 +12,25 @@ for(let value of arr){
     console.log(value)
 }
 
+
+
+
+
+
+
+
+
+
 //----Get slice in array----//
+
 //kono array er slice ber korar smy main array change hoy na, borong slice kora ongso ke new array te return kore dey//
 let array = [11,12,26,87,90,22,87,689,91,20,55];
 console.log(array.slice(1,3));
 
+
+
 //----splice in array----//
+
 //kono array er modde jodi amra splice kore dei tahole se orginal array kei change kore dey//
 let result = [33,77,56,90,67,61];
 //let result1 = result.splice(1,4);
