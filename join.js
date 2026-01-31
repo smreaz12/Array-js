7616a0814d85a1f3a92ffbff7990be7698576a70
smreaz12 <smreaz555@gmail.com>
2026-01-31 16:04:03 +0600

    join in array

---
+++ join.js
@@ -0,0 +1,12 @@
+//---join in array---//
+//basically join korar maddomeo string pawa jay//
+
+let nam = ["Reaz","Rahin","Rohan"];
+
+// console.log(nam.join(" "))
+// console.log(nam.join(","))
+// console.log(nam.join("-"))
+
+
+let join_nam = nam.join(",");
+console.log(join_nam);
